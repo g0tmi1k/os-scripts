875576e48af6e7ab48f27c3dc40a1522e3020779
Matt Gough <mpgcomp@gmail.com>
2016-01-31 12:56:47 +0000

    Update

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # grab the postkali config script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali
@@ -16,3 +16,4 @@ Terminal=false
 Type=Application
 
 EOF
+chmod +x /root/postkali
