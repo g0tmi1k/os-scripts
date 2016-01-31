ba500d7490f94071960788f7747426ab92a80a66
Matt Gough <mpgcomp@gmail.com>
2016-01-31 12:30:36 +0000

    Update

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,28 +1,18 @@
 #!/bin/sh
 
-# grab our firstboot script
+# grab the postkali config script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali
 chmod +x /root/postkali
 
-# create a service that will run our firstboot script
-cat > /etc/init.d/postkali <<EOF
-### BEGIN INIT INFO
-# Provides:        postkali
-# Required-Start:  $networking
-# Required-Stop:   $networking
-# Default-Start:   2 3 4 5
-# Default-Stop:    0 1 6
-# Short-Description: A script that runs once
-# Description: A script that runs once
-### END INIT INFO
-
-cd /root ; /usr/bin/nohup sh -x /root/postkali &
-
+# create a desktop shortcut to run the script
+cat > /root/Desktop/postkali.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Name=Postkali
+Comment=Configure Kali using Nettitude build script
+Exec=gnome-terminal -e /root/postkali
+Icon=utilities-terminal"' >> /root/Desktop/postKali.desktop
+Terminal=false
+Type=Application
 
 EOF
-
-# install the firstboot service
-chmod +x /etc/init.d/postkali
-update-rc.d postkali defaults
-
-echo "finished postinst of kali"
