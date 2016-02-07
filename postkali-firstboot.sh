3125bfe62d8eba503236c392040bfbedb4191787
Matt Gough <mpgcomp@gmail.com>
2016-02-07 16:56:57 +0000

    Update

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # grab the postkali config script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali
@@ -6,3 +6,4 @@ chmod +x /root/postkali
 # grab the postkali-icon to desktop and runit
 /usr/bin/wget -qO- http://bit.ly/postKali-icon > /root/Desktop/postKali-icon.desktop
 chmod +x /root/Desktop/postKali-icon.desktop
+exit 0
