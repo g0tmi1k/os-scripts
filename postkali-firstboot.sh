bea6bbfe6b55f96cd3d8d149a3c4f7c314356a67
Matt Gough <mpgcomp@gmail.com>
2016-02-08 17:49:51 +0000

    Updates

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 # grab the postkali-icon to desktop and runit
-mkdir /etc/skel/Desktop
-/usr/bin/wget -qO- http://bit.ly/postKali-icon > /etc/skel/Desktop/postKali-icon.desktop
-chmod +x /etc/skel/Desktop/postKali-icon.desktop
+/usr/bin/wget -qO- http://bit.ly/postKali-icon > /etc/xdg/autostart/postKali-icon.desktop
 
 # grab the postkali config script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali
