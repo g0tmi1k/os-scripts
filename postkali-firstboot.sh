9ff80a9c25b8d7917bca7d66b2ad67dfc1f775fd
Matt Gough <mpgcomp@gmail.com>
2016-02-09 08:59:09 +0000

    Update

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 # grab the postkali-icon to desktop and runit
-/usr/bin/wget -qO- http://bit.ly/postKali-icon > /etc/xdg/autostart/postKali-icon.desktop
-
+cat <<EOF > /etc/xdg/autostart/postkali.desktop
+[Desktop Entry]
+Version=1.0
+Name=Postkali
+Comment=Configure Kali using Nettitude build script
+Exec=gnome-terminal -e /root/postkali 2>&1 | tee /root/postKali-build.log; rm -f /etc/xdg/autostart/postkali.desktop
+Icon=utilities-terminal
+Terminal=false
+Type=Application
+X-GNOME-Autostart-enabled=true
+EOF
 # grab the postkali config script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali
 chmod +x /root/postkali
