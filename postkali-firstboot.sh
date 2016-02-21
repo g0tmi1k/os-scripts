9235771a56ac7131df3d6d82e8f8aec333f8df03
Matt Gough <mpgcomp@gmail.com>
2016-02-21 20:57:52 +0000

    Updates

--- postkali-firstboot.sh
+++ postkali-firstboot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# grab the postkali-icon to desktop and runit
+# Run the postkali-wrapper from autostart
 cat <<EOF > /etc/xdg/autostart/postkali.desktop
 [Desktop Entry]
 Version=1.0
@@ -12,14 +12,17 @@ Terminal=false
 Type=Application
 X-GNOME-Autostart-enabled=true
 EOF
+# Create the portkali-wrapper
 cat <<EOF > /root/postkali-wrapper.sh
 #!/bin/bash
 gnome-terminal -e /root/postkali.sh --teamviewer 2>&1 | tee /root/postKali-build.log
 rm -f /etc/xdg/autostart/postkali.desktop
 rm -f /etc/xdg/autostart/postkali-wrapper.sh
 EOF
+# Make it executable
 chmod +x /root/postkali-wrapper.sh
-# grab the postkali config script
+
+# Grab the main postkali script
 /usr/bin/wget -qO- http://bit.ly/postKali-netti2 > /root/postkali.sh
 chmod +x /root/postkali.sh
 
