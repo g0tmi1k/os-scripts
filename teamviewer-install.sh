308dfcc6eafa79062234a535cd3a84919e5d9cb1
Matt Gough <mpgcomp@gmail.com>
2016-03-07 18:20:14 +0000

    Update

--- teamviewer-install.sh
+++ teamviewer-install.sh
@@ -39,23 +39,23 @@ fi
 echo -e "\\n\\e[01;32m[+]\\e[00m Installing TeamviewerPro as a service"
 # Creating the password
  apt-get install -y -qq pwgen  || echo -e ' '${RED}'[!] Issue with apt-get'${RESET} 1>&2
- buildpwd_len=`shuf -i 8-12 -n 1`
- buildpwd=`pwgen -scn $buildpwd_len 1`
-   #--- Download TeamViewer and dependencies
-   dpkg --add-architecture i386
-   apt-get update || echo -e ' '${RED}'[!] Issue with adding i386 architecture'${RESET} 1>&2
-   apt-get -y -qq install libc6:i386 libgcc1:i386 libasound2:i386 libdbus-1-3:i386 libexpat1:i386 libfontconfig1:i386 libfreetype6:i386 libjpeg62:i386 libpng12-0:i386 libsm6:i386 libxdamage1:i386 libxext6:i386 libxfixes3:i386 libxinerama1:i386 libxrandr2:i386 libxrender1:i386 libxtst6:i386 zlib1g:i386 || echo -e ' '${RED}'[!] Issue installing TeamViewer dependencies'${RESET} 1>&2
-   wget http://download.teamviewer.com/download/teamviewer_i386.deb || echo -e ' '${RED}'[!] Issue downloading TeamViewer - check internet access'${RESET} 1>&2
-   dpkg -i teamviewer_i386.deb
-   #--- Configure TeamViewer
-   pause 20
-   teamviewer --daemon restart
-   teamviewer passwd $tvpwd 1>&2
-   teamviewer license accept 1>&2
-   teamviewer --daemon stop 1>&2
-   tvid=`teamviewer info |awk '/ID:/{print $5}'`
-   echo -e ""\\n\\e[01;32m[+]\\e[00m Teamviewer ID: $tvid$ Teamviewer Password: $tvpwd"
-   #--- Enable at boot
-    cp /opt/teamviewer/tv_bin/script/teamviewerd.sysv /etc/init.d/
-    chmod 755 /etc/init.d/teamviewerd.sysv
-echo -e "\\n\\e[01;32m[+]\\e[00m Done! Please send ID and Password to your Account Manager"
+ tvpwd_len=`shuf -i 8-12 -n 1`
+ tvpwd=`pwgen -scn $tvpwd_len 1`
+ #--- Download TeamViewer and dependencies
+ dpkg --add-architecture i386
+ apt-get update || echo -e ' '${RED}'[!] Issue with adding i386 architecture'${RESET} 1>&2
+ apt-get -y -qq install libc6:i386 libgcc1:i386 libasound2:i386 libdbus-1-3:i386 libexpat1:i386 libfontconfig1:i386 libfreetype6:i386 libjpeg62:i386 libpng12-0:i386 libsm6:i386 libxdamage1:i386 libxext6:i386 libxfixes3:i386 libxinerama1:i386 libxrandr2:i386 libxrender1:i386 libxtst6:i386 zlib1g:i386 || echo -e ' '${RED}'[!] Issue installing TeamViewer dependencies'${RESET} 1>&2
+ wget http://download.teamviewer.com/download/teamviewer_i386.deb || echo -e ' '${RED}'[!] Issue downloading TeamViewer - check internet access'${RESET} 1>&2
+ dpkg -i teamviewer_i386.deb
+ #--- Configure TeamViewer
+ teamviewer passwd $tvpwd 1>&2
+ teamviewer license accept 1>&2
+ teamviewer --daemon stop 1>&2
+ echo -e "\\n\\e[01;32m[+]\\e[00m Teamviewer ID: $tvid Teamviewer Password: $tvpwd"
+ #--- Enable at boot
+  cp /opt/teamviewer/tv_bin/script/teamviewerd.sysv /etc/init.d/
+  chmod 755 /etc/init.d/teamviewerd.sysv
+ teamviewer --daemon start
+ tvid=`teamviewer info |awk '/ID:/{print $5}'`
+ echo -e "\\n\\e[01;32m[+]\\e[00m Done! Please send ID and Password to your Account Manager"
+exit 0
